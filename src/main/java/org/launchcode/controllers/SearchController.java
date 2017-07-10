f6a5343a36c82c5c9e61b46f17abc5e705a2bfe1
Lynn Josse <lynnjosse@sbcglobal.net>
2017-07-09 22:50:05 -0500

    it may all work

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,21 +22,26 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
-
     @RequestMapping(value = "/results")
 
     public String searchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs;
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
 
-        model.addAttribute ("columns", ListController.columnChoices);
-        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
-        model.addAttribute("jobs", jobs);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
 
-        model.addAttribute ("resultsSize", jobs.size());
+        }
+        String resultMessage = jobs.size() + " Result(s)";
+        model.addAttribute ("resultMessage",resultMessage);
         return "search";
 
-    }
-
-
 
     }
+
+
+    }
+
